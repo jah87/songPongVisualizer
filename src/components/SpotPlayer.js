ba0f45dc98ca9965a2ecb8d64fb876060fd6113f
Jon <jonathan.anderson.hill@gmail.com>
2017-05-23 16:49:54 -0400

    cleaned up bottom bar, trying hide song cards by device size

--- src/components/SpotPlayer.js
+++ src/components/SpotPlayer.js
@@ -21,15 +21,18 @@ class SpotPlayer extends Component {
             <div className="fixed-bottom navbar-inverse bg-inverse">
 
                 <h5 className="whiteText text-center spaceTop"><i>Current Song:</i> <b>{song.title}</b></h5>
-                <div className="text-center flex-box spaceBot">
+                <div className="text-center flex-box player-box">
                 
-                <img src="/icons/icon_arrow_left.svg" onClick={()=>{this.clickPrev()}} className="arrowMargins" ></img>
+                    <img src="/icons/icon_arrow_left.svg" onClick={()=>{this.clickPrev()}} className="arrowMargins" alt="Previous Button" />
+
                     <audio src={song.source} autoPlay={this.props.songPlaying === false ? false : true}  
                     controls onPause={()=>{this.songPlayState(false)}} 
                     onPlay={()=>{this.songPlayState(true)}} id="audioPlayer">
                         Your browser does not support the audio tag.
                     </audio>
-                <img src="/icons/icon_arrow_right.svg" onClick={()=>{this.clickNext()}} className="arrowMargins" />
+
+                    <img src="/icons/icon_arrow_right.svg" onClick={()=>{this.clickNext()}} className="arrowMargins" alt="Next Button" />
+
                 </div>
                 
             </div>
